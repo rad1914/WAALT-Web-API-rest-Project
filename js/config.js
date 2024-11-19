27fd7bbf10c6a29df07e496926532276a94cea47
root <root@kali.my.domain>
2024-11-19 13:24:58 +0000

    commit

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// *config.js
+
+export const apiUrls = [
+    'https://wrldradd.loca.lt',
+    'https://wrldrad1914.loca.lt',
+];
+
+export const timeoutDuration = 45000; // Timeout duration in milliseconds
